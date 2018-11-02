cfa9f86ef726b4353f2284e0e6933c1a05ca860f
HorusDjer <eobineme@gmail.com>
2018-11-02 17:04:39 -0400

    added simple script

---
+++ remove_double_space.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int c;
+    int lastc
+
+    while(c = (getchar() != EOF)) {
+        if (c == ' '|| lastc != ' ')
+            putchar(c);
+    
+    lastc = c
+    }
+    
+    return 0;
+}
+
